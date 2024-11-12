02f4acaec680ea2a863fb5e3e8addd1b4f000d5e
Ali Zarringhalam <ali.zaringhalam@gmail.com>
2024-11-11 20:52:25 -0500

    fixed missing quotes around pred_vars in the runner script

--- dps/alg_3-4/run_boreal_biomass.sh
+++ dps/alg_3-4/run_boreal_biomass.sh
@@ -57,7 +57,7 @@ echo $ATL08_SAMPLE_CSV
 source activate r
 
 # Run mapBoreal with merged CSV as input
-cmd="Rscript ${basedir}/../../lib/mapBoreal_simple.R -a ${MERGED_ATL08_CSV} -t ${TOPO_TIF} -h ${HLS_TIF} -l ${LC_TIF} -m ${DO_SLOPE_VALID_MASK} -b ${ATL08_SAMPLE_CSV} -i ${iters} --minDOY ${minDOY} --maxDOY ${maxDOY} -s ${max_sol_el} -e ${expand_training} -p ${local_train_perc} --min_samples ${min_n} -v ${boreal_vect_fn} --predict_var ${predict_var} --max_samples ${max_n} --pred_vars ${pred_vars}"
+cmd="Rscript ${basedir}/../../lib/mapBoreal_simple.R -a ${MERGED_ATL08_CSV} -t ${TOPO_TIF} -h ${HLS_TIF} -l ${LC_TIF} -m ${DO_SLOPE_VALID_MASK} -b ${ATL08_SAMPLE_CSV} -i ${iters} --minDOY ${minDOY} --maxDOY ${maxDOY} -s ${max_sol_el} -e ${expand_training} -p ${local_train_perc} --min_samples ${min_n} -v ${boreal_vect_fn} --predict_var ${predict_var} --max_samples ${max_n} --pred_vars \"${pred_vars}\""
 
 echo $cmd
 eval $cmd
