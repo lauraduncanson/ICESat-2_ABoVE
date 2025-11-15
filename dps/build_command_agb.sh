f1625fa16780cb5cf69caf80795347213a61f00c
aliz237 <ali.zaringhalam@gmail.com>
2025-11-14 20:56:52 -0800

    updated build command for AGB runs

--- dps/build_command_agb.sh
+++ dps/build_command_agb.sh
@@ -1,19 +1,9 @@
-#!/bin/bash
-set -x
+#!/usr/bin/env -S bash --login
+set -euo pipefail
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
-
-#install requirements packages
 conda env update -f ${basedir}/above_env_r_3.1.4.yml
 
-pushd ${HOME}
 
-# Do not remove this (PMM Dec 2022)
-source activate icesat2_boreal
 
-# needed for ee asset export
-#mamba install --name icesat2_boreal -c conda-forge earthengine-api
 
-pip install git+https://github.com/MAAP-Project/maap-py.git#egg=maappy
 
-source activate r
-pip3 install pyOpenSSL --upgrade
